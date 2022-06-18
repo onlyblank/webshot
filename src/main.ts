7174959cd8e4746634ab3824fe17847c9a67dc09
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-06-18 21:41:18 +0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+function setupSwagger(app: INestApplication) {
+    const config = new DocumentBuilder()
+        .setTitle('Webshot')
+        .setDescription('Web page screenshot service')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    setupSwagger(app);
+
     await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
