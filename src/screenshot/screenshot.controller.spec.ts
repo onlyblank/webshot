eacb96af1b3e9c0a1233958cabfd44d1885463a3
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-07-05 18:17:38 +0300

    Create screenshot.controller.spec.ts

---
+++ src/screenshot/screenshot.controller.spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ScreenshotController } from './screenshot.controller';
+import { ScreenshotService } from './screenshot.service';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+
+describe('ScreenshotService', () => {
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [ScreenshotController],
+            providers: [ScreenshotService],
+        }).compile();
+
+        app = moduleRef.createNestApplication();
+        await app.init();
+    });
+
+    describe('POST /screenshot', () => {
+        test('request with empty body', () => {
+            return request(app.getHttpServer()).post('/screenshot').expect(400);
+        });
+    });
+});
