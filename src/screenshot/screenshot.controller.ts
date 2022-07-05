4615ab84220c771cc8b73df2f1576c6859e736f8
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-07-05 18:17:28 +0300

    Update screenshot.controller.ts

--- src/screenshot/screenshot.controller.ts
+++ src/screenshot/screenshot.controller.ts
@@ -1,9 +1,46 @@
-import { Controller, Post } from '@nestjs/common';
-
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Post,
+    Res,
+    StreamableFile,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
+import { ScreenshotDto } from './screenshot.dto';
+import { ScreenshotService } from './screenshot.service';
+import { Page } from 'puppeteer';
+import { Response } from 'express';
 @Controller('screenshot')
 export class ScreenshotController {
+    constructor(private screenshotService: ScreenshotService) {}
+
     @Post()
-    async main() {
-        return 'response';
+    @UsePipes(ValidationPipe)
+    async main(
+        @Body() screenshotDto: ScreenshotDto,
+        @Res({ passthrough: true }) res: Response
+    ): Promise<StreamableFile> {
+        let page: Page;
+        try {
+            page = await this.screenshotService.openPage(
+                screenshotDto.url,
+                screenshotDto.waitForEvent ?? false
+            );
+        } catch (err) {
+            console.error(err);
+            throw new BadRequestException(err);
+        }
+
+        const buffer = await this.screenshotService.screenshot(page, {
+            selector: screenshotDto.clipSelector,
+        });
+
+        res.set({
+            'Content-Type': 'image/png',
+            'Content-Length': buffer.length,
+        });
+        return new StreamableFile(buffer);
     }
 }
