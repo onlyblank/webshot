975a5b65e2128d2c0d6d794030da28537b4fad3f
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-07-05 09:07:20 +0300

    Added `rejectOnTimeout` tests

---
+++ src/screenshot/screenshot.service.spec.ts
@@ -0,0 +1,33 @@
+import { ScreenshotService } from './screenshot.service';
+
+describe('ScreenshotService', () => {
+    let screenshotService: ScreenshotService;
+
+    beforeAll(async () => {
+        screenshotService = new ScreenshotService();
+    });
+
+    describe('rejectOnTimeout', () => {
+        test('immediate function call', async () => {
+            const promise = Promise.resolve(3);
+            expect(await screenshotService.rejectOnTimeout(promise, 50)).toBe(
+                3
+            );
+        });
+
+        test('continous execution but within timeout', async () => {
+            const promise = new Promise(res => setTimeout(() => res(4), 25));
+            expect(await screenshotService.rejectOnTimeout(promise, 50)).toBe(
+                4
+            );
+        });
+
+        test('continous execution with exceeded timeout', async () => {
+            const promise = new Promise(res => setTimeout(() => res(4), 50));
+
+            await expect(
+                screenshotService.rejectOnTimeout(promise, 25)
+            ).rejects.toMatch('time limit exceeded');
+        });
+    });
+});
